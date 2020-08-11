ee602adad12d7ca51b528d3b00aa46cf2737764d
jabzyku <jabzyku@wp.pl>
2020-08-11 19:44:42 +0200

    improvement od webhooks

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 echo "wynik mojego skryptu" > wynik.txt
 echo "czy to działa????? Ale czy na pewno" >> wynik.txt
-echo "A może teraz!!!!!!!!!!!!!!!!!!!" >>wynik.txt
+echo "A może teraz!!!!!!!!!!!!!!!!!!!???????" >>wynik.txt
