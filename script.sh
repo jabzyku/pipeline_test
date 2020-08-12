dc3a7670ef84a3c0755f122ec2a028cc8118cf99
jabzyku <jabzyku@wp.pl>
2020-08-12 09:45:07 +0200

    test ze skryptu

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ echo "A może teraz!!!!!!!!!!!!!!!!!!!???????" >>wynik.txt
 echo " !!Zadziałaj Pajacu!!!!!!!!!!" >> wynik.txt
 echo "i jeszcze 1 2 3 4 5 6 7 8">>wynik.txt
 echo "moze dzisiaj 1 2 3 4" >> wynik.txt
-echo "NO TERAZ MUSI DZIALAC!!!!" >>wynik.txt
+echo "NO TERAZ MUSI DZIALAC!!!!!" >>wynik.txt
